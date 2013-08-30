706b9066be49bf5c98de734f34e1ebc4a787d8c0
Christopher Wellons <wellons@nullprogram.com>
2013-08-30 19:00:41 -0400

    Flesh out some elfeed-web docs.

--- web/elfeed-web.el
+++ web/elfeed-web.el
@@ -4,30 +4,35 @@
 
 ;;; Commentary:
 
+;; This is a very early work in progress. The long-term goal is to
+;; provide a web interface view of the database with optional remote
+;; tag updating. An AngularJS client accesses the database over a few
+;; RESTful endpoints with JSON for serialization.
+
+;; The IDs provided by RSS and Atom are completely arbitrary. To avoid
+;; ugly encoding issues they're normalized into short, unique,
+;; alphanumeric codes called webids. Both feeds and entries fall into
+;; the same webid namespace so they share a single endpoint.
+
 ;; Endpoints:
 
 ;; /elfeed/<path>
-
-;; Serves the static HTML, JS, and CSS content.
+;;     Serves the static HTML, JS, and CSS content.
 
 ;; /elfeed/content/<ref-id>
-
-;;   Serves content from the content database (`elfeed-deref').
+;;     Serves content from the content database (`elfeed-deref').
 
 ;; /elfeed/thing/<webid>
-
-;;   Serve up an elfeed-feed or elfeed-entry in JSON format.
+;;     Serve up an elfeed-feed or elfeed-entry in JSON format.
 
 ;; /elfeed/search
-
-;;   Accepts a q parameter which is an filter string to be parsed and
-;;   handled by `elfeed-search-parse-filter'.
+;;     Accepts a q parameter which is an filter string to be parsed
+;;     and handled by `elfeed-search-parse-filter'.
 
 ;; /elfeed/update
-
-;;   Accepts a time parameter. If time < `elfeed-db-last-update',
-;;   respond with time. Otherwise don't respond until database
-;;   updates (long poll).
+;;     Accepts a time parameter. If time < `elfeed-db-last-update',
+;;     respond with time. Otherwise don't respond until database
+;;     updates (long poll).
 
 ;;; Code:
 
