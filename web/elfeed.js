74a29472584f5e281b4a924945c5581eca71f70e
Christopher Wellons <wellons@nullprogram.com>
2013-08-30 11:22:07 -0400

    Switch to live filtering.

--- web/elfeed.js
+++ web/elfeed.js
@@ -5,26 +5,39 @@ function favicon(url) {
         .toString().replace(/\?$/, '');
 }
 
+function entryFill(entry) {
+    entry.favicon = favicon(entry.link);
+    var date = new Date(entry.date);
+    entry.dateString = [
+        1900 + date.getYear(),
+        1 + date.getMonth(),
+        date.getDate()
+    ].join('-');
+    entry.classes = entry.tags.map(function(tag) {
+        return 'tag-' + tag;
+    }).join(' ');
+}
+
 function SearchCtrl($scope, $http) {
     $scope.query = INITIAL_QUERY;
+    $scope.busy = false;
+    $scope.dirty = true;
+
     $scope.update = function() {
-        $http.get(URI('/elfeed/search').search({
-            q: $scope.query
-        }).toString()).success(function(data) {
-            data.map(function(entry) {
-                entry.favicon = favicon(entry.link);
-                var date = new Date(entry.date);
-                entry.dateString = [
-                    1900 + date.getYear(),
-                    1 + date.getMonth(),
-                    date.getDate()
-                ].join('-');
-                entry.classes = entry.tags.map(function(tag) {
-                    return 'tag-' + tag;
-                }).join(' ');
+        if (!$scope.busy) {
+            $scope.busy = true;
+            $scope.dirty = false;
+            $http.get(URI('/elfeed/search').search({
+                q: $scope.query
+            }).toString()).success(function(data) {
+                data.forEach(entryFill);
+                $scope.entries = data;
+                $scope.busy = false;
+                if ($scope.dirty) $scope.update();
             });
-            $scope.entries = data;
-        });
+        } else {
+            $scope.dirty = true;
+        }
     };
     $scope.update();
 }
