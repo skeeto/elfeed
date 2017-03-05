45dc25d00cc7592ac729151885b6b0c571e561fd
Christopher Wellons <wellons@nullprogram.com>
2017-03-05 11:42:30 -0500

    Add notes about what to update before releasing a new version.

--- elfeed.el
+++ elfeed.el
@@ -10,6 +10,13 @@
 ;; Elfeed is a web feed client for Emacs, inspired by notmuch. See
 ;; the README for full documentation.
 
+;; Notice: Before stamping a new release the following places need to
+;; be updated:
+;; * elfeed.el (`elfeed-version')
+;; * elfeed-pkg.el
+;; * web/elfeed-web-pkg.el
+;; * Makefile (VERSION)
+
 ;;; History:
 
 ;; Version 2.1.0: features and new database format
