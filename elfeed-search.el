cc69601b1f459d2faf535b10ac576d1a76805f30
Daniel Mendler <mail@daniel-mendler.de>
2026-04-30 21:34:59 +0200

    elfeed-search-print-entry--default: Improve last change

--- elfeed-search.el
+++ elfeed-search.el
@@ -360,8 +360,8 @@ The customization `elfeed-search-date-format' sets the formatting."
                                title-width
                                elfeed-search-title-max-width)
                         :left)))
-    (insert (propertize date 'face 'elfeed-search-date-face))
-    (insert " " (propertize title-column 'face title-faces 'kbd-help title))
+    (insert (propertize date 'face 'elfeed-search-date-face) " "
+            (propertize title-column 'face title-faces 'kbd-help title))
     (when feed-title
       (insert " " (propertize feed-title 'face 'elfeed-search-feed-face)))
     (when tags
